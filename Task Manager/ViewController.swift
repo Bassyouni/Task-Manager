78f792ba2d26f1277653bb02c58c7c34d4f2d564
Omar Bassyouni <obassiony@tacme.com>
2019-05-24 13:18:06 +0200

    Initial Commit

---
+++ Task Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task Manager
+//
+//  Created by Bassyouni on 5/24/19.
+//  Copyright © 2019 Bassyouni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
